f48174c19eacf196e1272137547031807a500d9a
OlegatorLE <oleh.travin@gmail.com>
2024-02-27 10:52:51 +0200

    Added django project with two models

---
+++ movies/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class Actor(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    release_year = models.IntegerField()
+    director = models.CharField(max_length=100)
+    actors = models.ManyToManyField(Actor)
+
+    def __str__(self):
+        return self.title
